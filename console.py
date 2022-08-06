1a5098f5dc2ad2fa51e975264ac5c8a9046641f4
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2022-08-06 14:33:06 +0300

    Update console.py

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ Module console
 Entry point of the command interpreter
 """
 import cmd
+import re
+from shlex import split
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
@@ -13,7 +15,24 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+    
+    
 class HBNBCommand(cmd.Cmd):
     """ The AirBnB command line interpreter """
     prompt = "(hbnb) "
